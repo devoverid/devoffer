5710c64a3f32be021465c4a2c47f7ab48b508fc2
zuramai <saugi.dev@gmail.com>
2025-08-11 20:20:51 +0700

    feat: add role utils

---
+++ src/utils/roles.ts
@@ -0,0 +1,17 @@
+import { GuildMember, GuildMemberRoleManager, Role } from "discord.js"
+import { ROLES } from "../constants"
+
+export const checkStreakCount = (streak_count: number) => {
+    const role = ROLES.find((role) => streak_count == role.threshold)
+    return role
+}
+
+export const addMemberRole = async (member: GuildMember, roleId: string) => {
+    return await member.roles.add(roleId)
+}
+
+export const resetMemberRoles =  (member: GuildMember) => {
+    ROLES.forEach(r => {
+        member.roles.remove(r.id)
+    })    
+}
