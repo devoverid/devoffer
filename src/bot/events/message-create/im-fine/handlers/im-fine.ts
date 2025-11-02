1745a79b0fdd6de9f115988adec86966d61b836d
alfianchii <pianaee379@gmail.com>
2025-11-02 18:48:37 +0700

    fix: `message-create` and `ready` events without guild assertion

--- src/bot/events/message-create/im-fine/handlers/im-fine.ts
+++ src/bot/events/message-create/im-fine/handlers/im-fine.ts
@@ -1,9 +1,6 @@
 import type { Event } from '@events/event'
-import type { Interaction, Message } from 'discord.js'
-import { sendReply } from '@utils/discord'
+import type { Message } from 'discord.js'
 import { DiscordBaseError } from '@utils/discord/error'
-import { log } from '@utils/logger'
-import { ImFine } from '../validators/im-fine'
 
 export class ImFineError extends DiscordBaseError {
     constructor(message: string, options?: { cause?: unknown }) {
@@ -14,18 +11,8 @@ export class ImFineError extends DiscordBaseError {
 export default {
     name: 'messageCreate',
     desc: 'Replying to a user when the user\'s chat contains \'fine\' word',
-    async exec(_, interaction: Interaction, msg: Message) {
-        try {
-            if (!interaction.inCachedGuild())
-                throw new ImFineError(ImFine.ERR.NotGuild)
-
-            if (!msg.author.bot && msg.content.includes('fine'))
-                await msg.reply('gua i\'m fine😅')
-        }
-        catch (err: any) {
-            if (err instanceof DiscordBaseError)
-                await sendReply(interaction, err.message)
-            else log.error(`Failed to handle: ${ImFine.ERR.UnexpectedImFine}: ${err}`)
-        }
+    async exec(_, msg: Message) {
+        if (!msg.author.bot && msg.content.includes('fine'))
+            await msg.reply('gua i\'m fine😅')
     },
 } as Event
