0c42811b389d1946ebe8acd321567d8122691d23
alfianchii <pianaee379@gmail.com>
2025-11-26 18:14:33 +0700

    fix: deprecated type

--- src/bot/events/event.d.ts
+++ src/bot/events/event.d.ts
@@ -1,7 +1,7 @@
-import type { Client } from 'discord.js'
+import type { Client, ClientEvents } from 'discord.js'
 
-export interface Event {
-    name: string
+export interface Event<K extends keyof ClientEvents = keyof ClientEvents> {
+    name: K
     desc: string
     once?: boolean
     exec: (client: Client, ...args: any[]) => void
